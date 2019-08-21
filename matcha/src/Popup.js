e72f00b2fd27c9a46529e0e8736136441943c54f
Philippine SENTILHES <psentilh@e1r7p13.42.fr>
2019-08-21 16:08:16 +0200

    beg of debug for the sign up popup form

--- matcha/src/Popup.js
+++ matcha/src/Popup.js
@@ -3,7 +3,7 @@ import './Popup.css';
 
 class PopUp extends Component {
 	
-	nextStep = () => {
+	nextStep = (props) => {
 		let current_fs, next_fs, previous_fs; //fieldsets
 		let left, opacity, scale; //fieldset properties which we will animate
 		let animating; //flag to prevent quick multi-click glitches
@@ -12,79 +12,92 @@ class PopUp extends Component {
 			return false;
 		animating = true;
 		
-		current_fs = this.parent();
-		next_fs = this.parent().next();
+		current_fs = document.querySelector('fieldset')
+		console.log(current_fs);
+		next_fs = current_fs.nextElementSibling;
+		console.log(next_fs);
 		
-		//activate next step on progressbar using the index of next_fs
-		$("#progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+		// activate next step on progressbar using the index of next_fs
+		document.querySelectorAll('#progressbar li')[1].classList.add("active");
 		
 		//show the next fieldset
-		next_fs.show(); 
+		next_fs.style.display = "block"; 
+
+		@keyframes current_fs.fadeOutLeft {
+			from {
+			  opacity: 1;
+			}
+		  
+			to {
+			  opacity: 0;
+			  transform: translate3d(-100%, 0, 0);
+			}
+		  }
 		//hide the current fieldset with style
-		current_fs.animate({opacity: 0}, {
-			step: function(now, mx) {
-				//as the opacity of current_fs reduces to 0 - stored in "now"
-				//1. scale current_fs down to 80%
-				scale = 1 - (1 - now) * 0.2;
-				//2. bring next_fs from the right(50%)
-				left = (now * 50)+"%";
-				//3. increase opacity of next_fs to 1 as it moves in
-				opacity = 1 - now;
-				current_fs.css({
-	        'transform': 'scale('+scale+')',
-	        'position': 'absolute'
-	      });
-				next_fs.css({'left': left, 'opacity': opacity});
-			}, 
-			duration: 800, 
-			complete: function(){
-				current_fs.hide();
-				animating = false;
-			}, 
+		// current_fs.animate({opacity: 0}, {
+		// 	step: function(now, mx) {
+		// 		//as the opacity of current_fs reduces to 0 - stored in "now"
+		// 		//1. scale current_fs down to 80%
+		// 		scale = 1 - (1 - now) * 0.2;
+		// 		//2. bring next_fs from the right(50%)
+		// 		left = (now * 50)+"%";
+		// 		//3. increase opacity of next_fs to 1 as it moves in
+		// 		opacity = 1 - now;
+		// 		current_fs.style = {
+	    //     		'transform': 'scale('+scale+')',
+	    //     		'position': 'absolute'
+	    //   		};
+		// 		next_fs.style = {'left': left, 'opacity': opacity};
+		// 	}, 
+		// 	duration: 800, 
+		// 	complete: function(){
+		// 		current_fs.style.display = 'none';
+		// 		animating = false;
+		// 	}, 
 			//this comes from the custom easing plugin
-			easing: 'easeInOutBack'
+			// easing: 'easeInOutBack'
 		});
 	}
-}
 
-$(".previous").click(function(){
-	if(animating) return false;
-	animating = true;
-	
-	current_fs = $(this).parent();
-	previous_fs = $(this).parent().prev();
-	
-	//de-activate current step on progressbar
-	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
-	
-	//show the previous fieldset
-	previous_fs.show(); 
-	//hide the current fieldset with style
-	current_fs.animate({opacity: 0}, {
-		step: function(now, mx) {
-			//as the opacity of current_fs reduces to 0 - stored in "now"
-			//1. scale previous_fs from 80% to 100%
-			scale = 0.8 + (1 - now) * 0.2;
-			//2. take current_fs to the right(50%) - from 0%
-			left = ((1-now) * 50)+"%";
-			//3. increase opacity of previous_fs to 1 as it moves in
-			opacity = 1 - now;
-			current_fs.css({'left': left});
-			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
-		}, 
-		duration: 800, 
-		complete: function(){
-			current_fs.hide();
-			animating = false;
-		}, 
-		//this comes from the custom easing plugin
-		easing: 'easeInOutBack'
-	});
-});
 
-$(".submit").click(function(){
-	return false;
-})
+	// previousStep = () => {
+	// 	if (animating) return false;
+	// 	animating = true;
+		
+	// 	current_fs = $(this).parent();
+	// 	previous_fs = $(this).parent().prev();
+		
+	// 	//de-activate current step on progressbar
+	// 	$("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+		
+	// 	//show the previous fieldset
+	// 	previous_fs.show(); 
+	// 	//hide the current fieldset with style
+	// 	current_fs.animate({opacity: 0}, {
+	// 		step: function(now, mx) {
+	// 			//as the opacity of current_fs reduces to 0 - stored in "now"
+	// 			//1. scale previous_fs from 80% to 100%
+	// 			scale = 0.8 + (1 - now) * 0.2;
+	// 			//2. take current_fs to the right(50%) - from 0%
+	// 			left = ((1-now) * 50)+"%";
+	// 			//3. increase opacity of previous_fs to 1 as it moves in
+	// 			opacity = 1 - now;
+	// 			current_fs.css({'left': left});
+	// 			previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+	// 		}, 
+	// 		duration: 800, 
+	// 		complete: function(){
+	// 			current_fs.hide();
+	// 			animating = false;
+	// 		}, 
+	// 		//this comes from the custom easing plugin
+	// 		easing: 'easeInOutBack'
+	// 	});
+	// }
+
+	// submit = () => {
+	// 	return false;
+	// }
 	
 	render() {
 		return (
@@ -102,7 +115,7 @@ $(".submit").click(function(){
 							<input type="text" name="email" placeholder="Email" />
 							<input type="password" name="pass" placeholder="Password" />
 							<input type="password" name="cpass" placeholder="Confirm Password" />
-							<input type="button" name="next" className="next action-button" value="Next" onClick={this.nextStep} />
+							<input type="button" name="next" className="next action-button" value="Next" onClick={this.nextStep.bind(this)} />
 						</fieldset>
 						<fieldset>
 							<h2 className="fs-title">Social Profiles</h2>
@@ -110,7 +123,7 @@ $(".submit").click(function(){
 							<input type="text" name="twitter" placeholder="Twitter" />
 							<input type="text" name="facebook" placeholder="Facebook" />
 							<input type="text" name="gplus" placeholder="Google Plus" />
-							<input type="button" name="previous" className="previous action-button" value="Previous" />
+							<input type="button" name="previous" className="previous action-button" value="Previous" /*onClick={this.previousStep}*//>
 							<input type="button" name="next" className="next action-button" value="Next" />
 						</fieldset>
 						<fieldset>
@@ -121,7 +134,7 @@ $(".submit").click(function(){
 							<input type="text" name="phone" placeholder="Phone" />
 							<textarea name="address" placeholder="Address"></textarea>
 							<input type="button" name="previous" className="previous action-button" value="Previous" />
-							<input type="submit" name="submit" className="submit action-button" value="Submit" />
+							<input type="submit" name="submit" className="submit action-button" value="Submit" /*onClick={this.submit}*//>
 						</fieldset>
 					</form>
 					<button onClick={this.props.ClosePopup}>close me</button>
