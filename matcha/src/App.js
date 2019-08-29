1c238f1d15d9ad5118c97c7cda327523f423c16b
Philippine SENTILHES <psentilh@e1r3p7.42.fr>
2019-08-29 16:15:17 +0200

    form exercise

--- matcha/src/App.js
+++ matcha/src/App.js
@@ -92,20 +92,191 @@ import React from 'react'
 class App extends React.Component {
   constructor() {
     super()
-    this.state = {}
+    this.state = {
+      firstName: "", 
+      lastName: "",
+      age: "",
+      gender: "",
+      location: "",
+      dietRestrict: {
+        isVege: false,
+        isVegan: false, 
+        isKosher: false,
+        isHalal: false
+      }
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
   }
 
-  componentDidMount() {
-    fetch("https://swapi.co/api/people/1")
-      .then(response => response.json())
-      .then(data => console.log(data))
+  handleSubmit(event) {
+    console.log(event.target)
+    alert(
+      "First Name: " + event.target.firstName.value
+      + "\nLast Name: " + event.target.lastName.value
+      + "\nAge: " + event.target.age.value
+      + "\nGender: " + event.target.gender.value
+      + "\nLocation: " + event.target.location.value)
+      // + "\nDiatery restrictions: " + event.target.dietRestrict.value)
+    event.preventDefault()
+  }
+
+  handleChange(event) {
+    const {name, value, checked, type} = event.target
+    type === "checkbox" ?
+    this.setState(prevState => {
+      return {
+        dietRestrict: {
+          ...prevState.dietRestrict,
+          [name]: checked
+        }
+      }
+    })
+    : this.setState({
+      [name]: value
+    })
   }
 
   render() {
+
+    const style = {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center', 
+      marginTop: '5%'
+    }
+
     return (
-      <div>
-        Code goes here
-      </div>
+      <main>
+        <form style={style} onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={this.state.firstName}
+            name="firstName"
+            placeholder="First Name"
+            onChange={this.handleChange}
+          />
+          <input
+            type="text"
+            value={this.state.lastName}
+            name="lastName"
+            placeholder="Last Name"
+            onChange={this.handleChange}
+          />
+          <input
+            type="number"
+            // pattern="[0-9]*"
+            value={this.state.age}
+            name="age"
+            placeholder="Age"
+            onChange={this.handleChange}
+          />
+
+
+          <label>
+            <input
+              type="radio"
+              value="male"
+              name="gender"
+              checked={this.state.gender === "male"}
+              onChange={this.handleChange}
+            /> Male
+          </label>
+          <label>
+            <input
+              type="radio"
+              value="female"
+              name="gender"
+              checked={this.state.gender === "female"}
+              onChange={this.handleChange}
+            /> Female
+          </label>
+
+          <label>Destination:
+            <br />
+            <select
+              onChange={this.handleChange}
+              value={this.state.location}
+              name="location"
+            >
+              <option value="">-- Please choose a destination --</option>
+              <option value="japan">Japan</option>
+              <option value="usa">USA</option>
+              <option value="france">France</option>
+              <option value="spain">Spain</option>
+              <option value="mexico">Mexico</option>
+            </select>
+          </label>
+
+          <label>Diatery restrictions:
+            <br />
+            <label>
+              <input 
+                type="checkbox"
+                value={this.state.dietRestrict}
+                name="isVege"
+                onChange={this.handleChange}
+                checked={this.state.dietRestrict.isVege}
+              /> Vegetarian
+            </label>
+            <br />
+            <label>
+              <input 
+                type="checkbox"
+                value={this.state.dietRestrict}
+                name="isVegan"
+                onChange={this.handleChange}
+                checked={this.state.dietRestrict.isVegan}
+              /> Vegan
+            </label>
+            <br />
+            <label>
+              <input 
+                type="checkbox"
+                value={this.state.dietRestrict}
+                name="isKosher"
+                onChange={this.handleChange}
+                checked={this.state.dietRestrict.isKosher}
+              /> Kosher
+            </label>
+            <br />
+            <label>
+              <input 
+                type="checkbox"
+                value={this.state.dietRestrict}
+                name="isHalal"
+                onChange={this.handleChange}
+                checked={this.state.dietRestrict.isHalal}
+              /> Halal
+            </label>
+          </label>
+
+          
+          <br />
+          <button>Submit</button>
+        </form>
+
+        <hr />
+        <h2>Entered informations:</h2>
+        <p>Your name: {this.state.firstName} {this.state.lastName}</p>
+        <p>Your age: {this.state.age}</p>
+        <p>Your gender: {this.state.gender}</p>
+        <p>Your destination: {this.state.location}</p>
+        <p>
+          Your diatery restrictions:
+          {console.log(this.state.dietRestrict)}
+          {this.state.dietRestrict.length ?
+            this.state.dietRestrict.map(restrict => {
+              console.log(restrict)
+              // restrict.value ?
+              // restrict.name
+              // : null
+            })
+            : null
+          }
+        </p>
+      </main>
     )
   }
 }
