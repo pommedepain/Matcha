481607eabffcb5d45c525a66e04b77c673acca28
cajulien <cajulien@student.42.fr>
2019-11-04 17:23:07 +0100

    routes protected

--- API/src/middleware/identify.js
+++ API/src/middleware/identify.js
@@ -1,8 +1,8 @@
 const debug = require('debug')('middleware:identify');
 
 module.exports = (req, res, next) => {
-  if (res.locals.user.username !== res.locals.check.username && res.locals.user.isAdmin !== 'true') {
-    debug('Rejected request from:', res.locals.user.username, 'regarding :', res.locals.check.username);
+  if (res.locals.user.username !== res.locals.check && res.locals.user.isAdmin !== 'true') {
+    debug('Rejected request from:', res.locals.user.username, 'regarding :', res.locals.check);
     return res.status(403).send('Forbidden.');
   }
   return (next());
