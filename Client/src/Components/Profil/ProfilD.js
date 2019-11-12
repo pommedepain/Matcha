dafa18a479c291a86eeca4bc175d23f110b39944
Philippine SENTILHES <psentilh@e1r6p20.42.fr>
2019-11-12 15:52:30 +0100

    Profil almost done

--- Client/src/Components/Profil/ProfilD.js
+++ Client/src/Components/Profil/ProfilD.js
@@ -4,6 +4,9 @@ import cx from 'classnames';
 import classes from './Profil.module.css';
 import Hashtag from '../../Icons/hashtag';
 import MapContainer from '../Geolocalisation/MapContainer';
+import Input from '../Utils/Input/Input';
+import AlertBox from '../Utils/AlertBox/AlertBox';
+import Tags from '../Utils/Tags/Tags';
 
 const ProfilDummy = (props) => {
 	const user = props.JWT.data;
@@ -63,6 +66,15 @@ const ProfilDummy = (props) => {
 		gender = "Queer";
 	}
 
+	let formElementsArray = [];
+	// eslint-disable-next-line no-unused-vars
+	for (let key in props.orderForm) {
+		formElementsArray.push({
+			id: key,
+			config: props.orderForm[key]
+		});
+	}
+
 	return (
 		<div className={classes.main}>
 			<div className={classes.contener} >
@@ -108,55 +120,139 @@ const ProfilDummy = (props) => {
 						<div className={classes.rightTop}>
 							<div className={classes.basicInfos}>
 								<h3>{user.firstName} {user.lastName}</h3>
-								<h4 className={classes.usernameAge}>{user.username}, {user.age} yo</h4>
-								<h4 className={classes.genderSex}><i className={cx(genderIcon, classes.genderIcon)}></i>{gender}, {sexOrient}</h4>
+								{props.edit.active ?
+									[<div className={classes.ageEdit} key={0} >
+										<h4 className={classes.usernameAge}>{user.username}</h4>
+										<Input 
+											key={formElementsArray[0].id}
+											elementType={formElementsArray[0].config.elementType}
+											elementConfig={formElementsArray[0].config.elementConfig}
+											value={formElementsArray[0].config.value}
+											changed={(event) => props.inputChangedHandler(event, formElementsArray[0].id)}
+											invalid={!formElementsArray[0].config.valid}
+											shouldValidate={formElementsArray[0].config.validation}
+											touched={formElementsArray[0].config.touched}
+										/>
+									</div>]
+									: <h4 className={classes.usernameAge}>{user.username}, {user.age} yo</h4>
+								}
+								{props.edit.active ? 
+									[<div className={classes.genderSexualOrient} key={0} >
+										<Input 
+											key={formElementsArray[1].id}
+											elementType={formElementsArray[1].config.elementType}
+											elementConfig={formElementsArray[1].config.elementConfig}
+											value={formElementsArray[1].config.value}
+											changed={(event) => props.inputChangedHandler(event, formElementsArray[1].id)}
+											invalid={!formElementsArray[1].config.valid}
+											shouldValidate={formElementsArray[1].config.validation}
+											touched={formElementsArray[1].config.touched}
+										/>
+										<Input 
+											key={formElementsArray[2].id}
+											elementType={formElementsArray[2].config.elementType}
+											elementConfig={formElementsArray[2].config.elementConfig}
+											value={formElementsArray[2].config.value}
+											changed={(event) => props.inputChangedHandler(event, formElementsArray[2].id)}
+											invalid={!formElementsArray[2].config.valid}
+											shouldValidate={formElementsArray[2].config.validation}
+											touched={formElementsArray[2].config.touched}
+										/>
+									</div>]
+									: <h4 className={classes.genderSex}><i className={cx(genderIcon, classes.genderIcon)}></i>{gender}, {sexOrient}</h4> 
+								}
 							</div>
 						</div>
 						<div className={classes.rightMiddle}>
 							<i className={cx("fas fa-quote-left", classes.left_quote)}></i>
 							<div className={classes.bioCont}>
+							{props.showAlert ? 
+								<AlertBox 
+									message="Please choose a Tag amongst the suggestions"
+									button="Try Again"
+									color="red"
+									handleChange={props.handleChangeTags}
+								/>
+								: null
+							}
 								<div className={classes.IAm}>
 									<h5>I am...</h5>
-									<div className={classes.tagContIAm}>
-										{user.isTags.map((elem, i) => (
-											<span key={i} className={classes.price_tag}>
-												<Hashtag className={classes.hash}/> 
-												{elem.text}
-											</span>
-										))}
-									</div>
+									{props.edit.active ?
+										[<Tags 
+											styling={true}
+											title=""
+											divclassname={classes.IAmTags}
+											h3classname={classes.questionsS3}
+											tags={props.isTags}
+											handleDelete={props.handleDeleteIAm}
+											handleAddition={props.handleAdditionIAm}
+											key={0}
+										/>]
+										: [<div className={classes.tagContIAm} key={0}>
+											{user.isTags.map((elem, i) => (
+												<span key={i} className={classes.price_tag}>
+													<Hashtag className={classes.hash}/> 
+													{elem.text}
+												</span>
+											))}
+										</div>]
+									}
 								</div>
 								<div className={classes.bio}>
-									<h4>{user.bio}</h4>
+									{props.edit.active ?
+										[<Input 
+											key={formElementsArray[3].id}
+											elementType={formElementsArray[3].config.elementType}
+											elementConfig={formElementsArray[3].config.elementConfig}
+											value={formElementsArray[3].config.value}
+											changed={(event) => props.inputChangedHandler(event, formElementsArray[3].id)}
+											invalid={!formElementsArray[3].config.valid}
+											shouldValidate={formElementsArray[3].config.validation}
+											touched={formElementsArray[3].config.touched}
+										/>]
+										: <h4>{user.bio}</h4> 
+									}
 								</div>
 								<div className={classes.lookingFor}>
 									<h5>I'm interested In...</h5>
-									<div className={classes.tagContInterest}>
-										{user.lookTags.map((elem, i) => (
-											<span key={i} className={classes.price_tag}>
-												<Hashtag className={classes.hash}/> 
-												{elem.text}
-											</span>
-										))}
-									</div>
+									{props.edit.active ?
+										[<Tags 
+											styling={true}
+											title=""
+											divclassname={classes.lookingForTags}
+											h3classname={classes.questionsS3}
+											tags={props.lookTags}
+											handleDelete={props.handleDeleteLookFor}
+											handleAddition={props.handleAdditionLookFor}
+											key={0}
+										/>]
+										: [<div className={classes.tagContInterest} key={0} >
+											{user.lookTags.map((elem, i) => (
+												<span key={i} className={classes.price_tag}>
+													<Hashtag className={classes.hash}/> 
+													{elem.text}
+												</span>
+											))}
+										</div>]
+									}
 								</div>
 							</div>
 							<i className={cx("fas fa-quote-right", classes.right_quote)}></i>
 						</div>
 						<button
-							className={classes.edit}
+							className={cx(classes.edit, "edit")}
 							onClick={props.editProfil}
-						>Edit</button>
+						>{props.edit.active ? 'Save' : 'Edit'}</button>
 					</div>
 				</div>
 				<div className={classes.secondRow}>
+					<div className={classes.geolocCont} >
+						<MapContainer className={classes.map} />
+					</div>
 					<div className={classes.cercle}>
 						<div className={classes.matchIndicator}>
 							<h3 className={classes.percentage}>{user.popularity} <i className="fas fa-percentage"></i></h3>
 							<h3 className={classes.popularity}>popularity</h3>
-						</div>
-						<div className={classes.geolocCont} >
-							<MapContainer className={classes.map} />
 						</div>
 					</div>
 				</div>
--- Client/src/Components/Profil/ProfilS.js
+++ Client/src/Components/Profil/ProfilS.js
@@ -4,16 +4,297 @@ import { UserContext } from '../../Contexts/UserContext';
 import ProfilDummy from './ProfilD';
 import axios from 'axios';
 
+const TagDatas = require('../../Datas/tagSuggestions.json');
+const formDatas = require('../../Datas/profilForm.json');
+
 class Profil extends Component {
 	constructor () {
 		super();
 		this.state = {
 			file: '',
-			imagePreviewUrl: ''
+			imagePreviewUrl: '',
+			edit: {
+				active: false,
+				innerHTML: "Edit"
+			},
+			orderForm: formDatas.orderForm,
+			showAlert: false,
+			isTags: [],
+			isTagsTouched: false,
+			lookTags: [],
+			lookTagsTouched: false
 		};
 	}
 
 	static contextType = UserContext;
+
+	componentDidMount () {
+		let newInfos = this.state.orderForm;
+		if (this.context.JWT.data.bio !== undefined) {
+			newInfos.bio.value = this.context.JWT.data.bio;
+		}
+		else {
+			newInfos.bio.value = "";
+		}
+		newInfos.gender.value = this.context.JWT.data.gender;
+		newInfos.sexOrient.value = this.context.JWT.data.sexOrient;
+		newInfos.birthdate.value = this.context.JWT.data.birthdate;
+
+		this.setState({
+			isTags: this.context.JWT.data.isTags,
+			lookTags: this.context.JWT.data.lookTags,
+			orderForm: newInfos
+		}, function() { console.log(this.state) });
+	}
+
+	checkValidity2(value, rules, inputIdentifier) {
+		let isValid = true;
+		let errorMessages = [];
+
+			if (!rules) {
+				return (true);
+			}
+			if (rules.required) {
+				isValid = (value.trim() !== "") && isValid;
+				if (inputIdentifier !== undefined && value.trim() === "")
+				{	
+					errorMessages.push("This field is required");
+					return (errorMessages);
+				}
+			}
+			if (!rules.required) {
+				if ((value.trim() === "")) {
+					// console.log("not required");
+					return (true);
+				}
+			}
+			if (rules.minLength) {
+				isValid = (value.length >= rules.minLength) && isValid;
+				if (inputIdentifier !== undefined && (value.length < rules.minLength))
+				{	
+					errorMessages.push("This field requires at least " + rules.minLength + " characters");
+					return (errorMessages);
+				}
+			}
+			if (rules.maxLength) {
+				isValid = (value.length <= rules.maxLength) && isValid;
+				if (inputIdentifier !== undefined && (value.length > rules.maxLength))
+				{	
+					errorMessages.push("This field must not exceed " + rules.maxLength + " characters");
+					return (errorMessages);
+				}
+			}
+			if (rules.regex) {
+				isValid = RegExp(unescape(rules.regex), 'g').test(value) && isValid;
+				if (!isValid)
+				{
+					errorMessages.push(rules.rule);
+					return (errorMessages);
+				}
+			}
+		return (isValid);
+	}
+
+	checkValidity(value, rules, inputIdentifier, state) {
+		return new Promise (function (resolve, reject) {
+			let isValid = true;
+			let errorMessages = [];
+
+			if (!rules) {
+				resolve(true);
+			}
+			if (rules.required === true) {
+				isValid = (value.trim() !== "") && isValid;
+				if (inputIdentifier !== undefined && value.trim() === "")
+				{	
+					errorMessages.push("This field is required");
+					reject(errorMessages);
+				}
+			}
+			if (!rules.required) {
+				if ((value.trim() === "")) {
+					resolve(true);
+				}
+			}
+			if (rules.minLength) {
+				isValid = (value.length >= rules.minLength) && isValid;
+				if (inputIdentifier !== undefined && (value.length < rules.minLength))
+				{	
+					errorMessages.push("This field requires at least " + rules.minLength + " characters");
+					reject(errorMessages);
+				}
+			}
+			if (rules.maxLength) {
+				isValid = (value.length <= rules.maxLength) && isValid;
+				if (inputIdentifier !== undefined && (value.length > rules.maxLength))
+				{	
+					errorMessages.push("This field must not exceed " + rules.maxLength + " characters");
+					reject(errorMessages);
+				}
+			}
+			if (rules.regex) {
+				isValid = RegExp(unescape(rules.regex), 'g').test(value) && isValid;
+				if (inputIdentifier !== undefined && (RegExp(unescape(rules.regex), 'g').test(value) === false))
+				{	
+					errorMessages.push(rules.rule);
+					reject(errorMessages);
+				}
+			}
+			if(!rules.db && !rules.checkEmail) {
+				resolve(isValid);
+			}
+			if (rules.checkEmail === true) {
+				if (state.emails.includes(value) === false) {
+					isValid = true && isValid;
+					resolve(isValid);
+				}
+				else if (state.emails.includes(value) === true) {
+					isValid = false;
+					errorMessages.push("This e-mail adress is already being used");
+					reject(errorMessages);
+				}
+			}
+			if (rules.db === true) {
+				if (state.users.includes(value) === false) {
+					isValid = true && isValid;
+					resolve(isValid);
+				}
+				else if (state.users.includes(value) === true) {
+					errorMessages.push("Username already taken");
+					reject(errorMessages);
+				}
+			}
+		});
+	}
+
+	inputChangedHandler = (event, inputIdentifier) => {
+		let updatedOrderForm = {};
+		const trueOrderForm = inputIdentifier === "birthdate" || inputIdentifier === "gender" || inputIdentifier === "sexualOrient" || inputIdentifier === "bio" ? "orderForm2" : "orderForm1";
+
+		trueOrderForm === "orderForm2" ?
+		updatedOrderForm = { ...this.state.orderForm2 }
+		: updatedOrderForm = { ...this.state.orderForm1 };
+
+		const updatedFormElement = {
+			...updatedOrderForm[inputIdentifier]
+		};
+		updatedFormElement.value = event.target.value;
+		if (inputIdentifier !== "birthdate") {
+			this.checkValidity(updatedFormElement.value, updatedFormElement.validation, inputIdentifier, this.state)
+				.then((response) => {
+					// console.log(response);
+					updatedFormElement.valid = response;
+					updatedFormElement.touched = true;
+					updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+					let formIsValid = true;
+					// eslint-disable-next-line no-unused-vars
+					for (let inputIdentifier in updatedOrderForm) {
+						formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+					}
+					trueOrderForm === "orderForm2" ?
+					this.setState({ orderForm2: updatedOrderForm, formIsValid: formIsValid })
+					: this.setState({ orderForm1: updatedOrderForm, formIsValid: formIsValid });
+				})
+				.catch((e) => {
+					updatedFormElement.valid = false;
+					// console.log(e);
+					updatedFormElement.touched = true;
+					updatedFormElement.errorMessage = e;
+					updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+					let formIsValid = true;
+					// eslint-disable-next-line no-unused-vars
+					for (let inputIdentifier in updatedOrderForm) {
+						formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+					}
+					trueOrderForm === "orderForm2" ?
+					this.setState({ orderForm2: updatedOrderForm, formIsValid: formIsValid })
+					: this.setState({ orderForm1: updatedOrderForm, formIsValid: formIsValid });
+				})
+		}
+		else {
+			const ret = this.checkValidity2(updatedFormElement.value, updatedFormElement.validation);
+			if (typeof ret === "boolean") {
+				updatedFormElement.valid = ret;
+			}
+			else {
+				updatedFormElement.valid = false;
+				updatedFormElement.errorMessage = ret;
+			}
+			updatedFormElement.touched = true;
+			updatedOrderForm[inputIdentifier] = updatedFormElement;
+
+			let formIsValid = true;
+			// eslint-disable-next-line no-unused-vars
+			for (let inputIdentifier in updatedOrderForm) {
+				formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+			}
+			this.setState({ orderForm2: updatedOrderForm, formIsValid: formIsValid });
+		}
+	}
+
+	handleDeleteIAm = (i) => {
+		const { isTags } = this.state
+		this.setState({
+			isTagstouched: true,
+			isTags: isTags.filter((tag, index) => index !== i),
+		})
+	}
+
+	handleAdditionIAm = (tag) => {
+		for (let i = 0; i < TagDatas.suggestions.length; i++)
+		{
+			if (TagDatas.suggestions[i].text === tag.text)
+			{
+				return (this.setState({ 
+						isTags: [...this.state.isTags, tag],
+						isTagsTouched: true }, function() {console.log(this.state)})
+				);
+			}
+		}
+		return (
+			this.setState(
+				{ showAlert: true }
+			)
+		)
+	}
+
+	handleDeleteLookFor = (i) => {
+		const { lookTags } = this.state
+		this.setState({
+			lookTagstouched: true,
+			lookTags: lookTags.filter((tag, index) => index !== i),
+		})
+	}
+
+	handleAdditionLookFor = (tag) => {
+		for (let i = 0; i < TagDatas.suggestions.length; i++)
+		{
+			if (TagDatas.suggestions[i].text === tag.text)
+			{
+				return (this.setState({ 
+						lookTags: [...this.state.lookTags, tag],
+						lookTagsTouched: true }, function() {console.log(this.state)})
+				);
+			}
+		}
+		return (
+			this.setState(
+				{ showAlert: true }
+			)
+		)
+	}
+
+	handleChangeTags = (event) => {
+		if (event.type === 'click')
+		{
+			event.preventDefault();
+			this.setState({
+				showAlert: !this.state.showAlert
+			})
+		}
+	}
 
 	handleSubmit(e) {
 		e.preventDefault();
@@ -40,16 +321,30 @@ class Profil extends Component {
 	editProfil = (e) => {
 		e.preventDefault();
 		console.log("editProfil triggered");
-		
+		let newEdit = this.state.edit;
+		if (this.state.edit.active === false) {
+			newEdit.active = true;
+		}
+		else {
+			newEdit.active = false;
+		}
+		this.setState({ edit: newEdit });
 	}
 
 	render () {
-		console.log(this.context)
 		return (
 			<ProfilDummy
 				handleSubmit={this.handleSubmit.bind(this)}
 				handleImageChange={this.handleImageChange.bind(this)}
 				editProfil={this.editProfil.bind(this)}
+				checkValidity={this.checkValidity.bind(this)}
+				checkValidity2={this.checkValidity2.bind(this)}
+				inputChangedHandler={this.inputChangedHandler.bind(this)}
+				handleDeleteIAm={this.handleDeleteIAm.bind(this)}
+				handleAdditionIAm={this.handleAdditionIAm.bind(this)}
+				handleDeleteLookFor={this.handleDeleteLookFor.bind(this)}
+				handleAdditionLookFor={this.handleAdditionLookFor.bind(this)}
+				handleChangeTags={this.handleChangeTags.bind(this)}
 				{...this.state}
 				{...this.context}
 			/>
